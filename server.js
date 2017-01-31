2dbddb22b06082c24de41c32683d346760c738f7
pittcr3w <pittcrew@ksu.edu>
2017-01-31 14:21:28 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
- */ 
+ */
 var http = require('http');
 var fs = require('fs');
 var port = 30022;
+var stylesheet = fs.readFileSync('gallery.css');
+
+var imageNames = ['ace', 'bubble', 'chess', 'fern', 'mobile'];
 
 function serveImage(filename, req, res){
 	fs.readFile('images/' + filename, function(err, body){
@@ -19,15 +22,29 @@ function serveImage(filename, req, res){
 			return;
 		}
 	res.setHeader("Content-Type", "image/jpeg");
-	res.end(body);	
+	res.end(body);
 	});
 }
 
 
 var server = http.createServer((req, res) => {
-	
-	
+
+
 	switch(req.url){
+		case '/gallery':
+			var gHtml = imageNames.map(function(fileName){
+				return '<img src="' + fileName + '" alt="fishing ace at work">';
+			}).join('');
+			var html = "<!doctype HTML><head><title>Gallery</title>";
+			html += '<link href="gallery.css" rel="stylesheet" type="text/css"></head>';
+			html += "<body>";
+			html += "<h1>Gallery</h1>";
+			html += '<img src="ace" alt="fishing ace at work">';
+			html += "<h1>Hello.</h1>The time is " + Date.now();
+			html += "</body>";
+			res.setHeader('Content-Type', 'text/html');
+			res.end(gHtml);
+			break;
 		case "/chess":
 			serveImage('chess.jpg', req, res);
 			break;
@@ -42,6 +59,10 @@ var server = http.createServer((req, res) => {
 			break;
 		case "/mobile":
 			serveImage('mobile.jpg', req, res);
+			break;
+		case '/gallery.css':
+			res.setHeader('Content-Type', 'text/css');
+			res.end(stylesheet);
 			break;
 		default:
 			res.statusCode = 404;
